92541737dee0ddd467798d8ce02ec6828e9288cc
Pakalak3 <pakalak3@gmail.com>
2021-11-04 09:44:46 +0500

    Commit

---
+++ Components/ProfilePage.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react';
+import {Text, View} from 'react-native';
+import styles from './StyleSheet';
+
+const profilePage = () => {
+  const [data, setData] = useState([]);
+
+  const getInfo = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+      });
+  };
+
+  const renderMe = ({item}) => (
+    <View style={styles.container}>
+      <Text style={styles.textStyle1}> {item.name} </Text>
+      <Text style={styles.textStyle2}> {item.email} </Text>
+    </View>
+  );
+  useEffect(() => {
+    getInfo();
+  });
+
+  return (
+    <View style={styles.homeContainer}>
+      <Text style={({fontSize: 25, fontWeight: 'bold'}, {textAlign: 'center'})}>
+        Details
+      </Text>
+      <Text> </Text>
+      <View style={styles.listContainer}></View>
+    </View>
+  );
+};
+export default profilePage;
