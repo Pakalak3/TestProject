1b7091aef131877546c042bcb8229f3ede950238
Pakalak3 <pakalak3@gmail.com>
2021-11-05 21:14:33 +0500

    Commit

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomePage from './Homepage';
+import DetailsPage from './Details';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="Details" component={DetailsPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
